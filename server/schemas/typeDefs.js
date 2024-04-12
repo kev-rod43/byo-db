6d7f3039d0999697bbdab5e279876a39f601a48e
HunterBell512 <bellhunter512@gmail.com>
2024-04-11 20:07:26 -0400

    added basic typeDefs, queries and mutations

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,7 +1,42 @@
 const typeDefs = `
-    type Query {
-        name: String
+    type User {
+        _id: ID!
+        username: String!
+        email: String!
     }
+
+    type Product {
+        product_name: String!
+        stock: Int!
+        description: String
+        purchased: Float
+        price: Float
+        condition: String
+        shipping_properties: Shipping
+        tag: Tag
+    }
+
+    type Shipping {
+        height: Float
+        width: Float
+        depth: Float
+        weight: Float
+    }
+
+    type Tag {
+        tag_name: String!
+    }
+
+    type Collection {
+        collection_name: String!
+        products: [Product]
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+
 `;
 
-module.exports = typeDefs; 
+module.exports = typeDefs;
