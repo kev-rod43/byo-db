14ae751cfdbde14663444cd2085e588ca41450c0
Jeffrey Scott <82685075+vader9911@users.noreply.github.com>
2024-04-17 22:00:01 -0400

    update welcome box

--- client/src/components/common/Welcome.jsx
+++ client/src/components/common/Welcome.jsx
@@ -1,19 +1,34 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
+import React from 'react';
+import { Box, Button } from '@mui/material';
+import Typography from '@mui/material/Typography';
 
-export default function BoxTest() {
+export default function WelcomeBox() {
   return (
     <Box
-      height={200}
-      width={200}
+      height={300}
+      width={400} 
       my={4}
       display="flex"
+      flexDirection="column"  
+      justifyContent="center"
       alignItems="center"
-      gap={4}
+      gap={2} 
       p={2}
-      sx={{ border: '2px solid grey' }}
+      sx={{
+        border: '2px',
+        borderRadius: '16px', 
+        boxShadow: '0 2px 22px rgba(0, 0, 0, 0.1)', 
+        margin: 'auto', 
+        position: 'relative', 
+        top: '50%',
+        transform: 'translateY(-50%)' 
+      }}
     >
-      This Box uses MUI System props for quick customization.
+      <Typography variant="h5" component="h1" gutterBottom>
+        Welcome to Build Your own DB! 
+      </Typography>
+      <Button variant="contained" color="primary">Login</Button>
+      <Button variant="outlined" color="primary" >Signup</Button>
     </Box>
   );
 }
--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,13 +1,13 @@
 
 import React from "react";
-import BoxTest from "../components/common/Welcome"
+import WelcomeBox from "../components/common/Welcome"
 
 
 
 const LandingPage = () => {
 return(<>
     
-    <BoxTest/>
+    <WelcomeBox/>
     
 
 </>)
