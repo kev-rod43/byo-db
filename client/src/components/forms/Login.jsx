8d32dbc1a787189456c09b95be21d5d244c8093c
Jeffrey Scott <82685075+vader9911@users.noreply.github.com>
2024-04-18 20:29:06 -0400

    add form handling to welcome box on landing page

--- client/src/components/forms/Login.jsx
+++ client/src/components/forms/Login.jsx
@@ -6,13 +6,12 @@ import InputAdornment from '@mui/material/InputAdornment';
 import Visibility from '@mui/icons-material/Visibility';
 import VisibilityOff from '@mui/icons-material/VisibilityOff';
 import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
 
-export default function SignupForm() {
+function LoginForm({ onBack }) {
   const [values, setValues] = React.useState({
-    username: '',
     email: '',
     password: '',
-    confirmPassword: '',
     showPassword: false,
   });
 
@@ -28,6 +27,12 @@ export default function SignupForm() {
     event.preventDefault();
   };
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Add login logic here
+    console.log('Login Submitted', values);
+  };
+
   return (
     <Container component="main" maxWidth="xs">
       <Box
@@ -40,16 +45,8 @@ export default function SignupForm() {
         }}
         noValidate
         autoComplete="off"
+        onSubmit={handleSubmit}
       >
-        <TextField
-          fullWidth
-          required
-          id="username"
-          label="Username"
-          value={values.username}
-          onChange={handleChange('username')}
-          margin="normal"
-        />
         <TextField
           fullWidth
           required
@@ -82,30 +79,26 @@ export default function SignupForm() {
           }}
           margin="normal"
         />
-        <TextField
+        <Button
+          type="submit"
           fullWidth
-          required
-          id="confirmPassword"
-          label="Confirm Password"
-          type={values.showPassword ? 'text' : 'password'}
-          value={values.confirmPassword}
-          onChange={handleChange('confirmPassword')}
-          InputProps={{
-            endAdornment: (
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label="toggle password visibility"
-                  onClick={handleClickShowPassword}
-                  onMouseDown={handleMouseDownPassword}
-                >
-                  {values.showPassword ? <VisibilityOff /> : <Visibility />}
-                </IconButton>
-              </InputAdornment>
-            ),
-          }}
-          margin="normal"
-        />
+          variant="contained"
+          sx={{ mt: 3, mb: 2 }}
+        >
+          Log In
+        </Button>
+        <Button
+          onClick={onBack}
+          fullWidth
+          variant="text"
+          sx={{ mt: 1, mb: 2 }}
+        >
+          Back
+        </Button>
+        
       </Box>
     </Container>
   );
 }
+
+export default LoginForm;
--- client/src/components/forms/Signup.jsx
+++ client/src/components/forms/Signup.jsx
@@ -8,10 +8,12 @@ import VisibilityOff from '@mui/icons-material/VisibilityOff';
 import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
 
-export default function LoginForm() {
+function SignupForm( { onBack }) {
   const [values, setValues] = React.useState({
+    username: '',
     email: '',
     password: '',
+    confirmPassword: '',
     showPassword: false,
   });
 
@@ -27,12 +29,6 @@ export default function LoginForm() {
     event.preventDefault();
   };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    // Add login logic here
-    console.log('Login Submitted', values);
-  };
-
   return (
     <Container component="main" maxWidth="xs">
       <Box
@@ -41,12 +37,21 @@ export default function LoginForm() {
           display: 'flex',
           flexDirection: 'column',
           alignItems: 'center',
-          m: 4,
+          top: '20vh'
+          
         }}
         noValidate
         autoComplete="off"
-        onSubmit={handleSubmit}
       >
+        <TextField
+          fullWidth
+          required
+          id="username"
+          label="Username"
+          value={values.username}
+          onChange={handleChange('username')}
+          margin="normal"
+        />
         <TextField
           fullWidth
           required
@@ -79,15 +84,48 @@ export default function LoginForm() {
           }}
           margin="normal"
         />
+        <TextField
+          fullWidth
+          required
+          id="confirmPassword"
+          label="Confirm Password"
+          type={values.showPassword ? 'text' : 'password'}
+          value={values.confirmPassword}
+          onChange={handleChange('confirmPassword')}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                >
+                  {values.showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+          margin="normal"
+        />
         <Button
           type="submit"
           fullWidth
           variant="contained"
           sx={{ mt: 3, mb: 2 }}
         >
-          Log In
+          Sign Up
+        </Button>
+        <Button
+          onClick={onBack}
+          fullWidth
+          variant="text"
+          sx={{ mt: 1, mb: 2 }}
+        >
+          Back
         </Button>
       </Box>
     </Container>
   );
 }
+
+export default SignupForm
